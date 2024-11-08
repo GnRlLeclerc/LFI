095b56d03cd587fa04186fb33f82ae0e8f445a43
Thibaut <thibaut2saivre@gmail.com>
2024-11-08 16:59:13 +0100

    Implement the remaining metrics

--- metrics.py
+++ metrics.py
@@ -1,32 +1,72 @@
 """Metrics definitions for mLFHT methods."""
 
-from test import KernelFunction
+from typing import Literal
 
 import numpy as np
 
+from kernels import KernelFunction
 
-class T:
-    """Test statistic for mLFHT methods."""
 
-    def __init__(self, kernel: KernelFunction) -> None:
-        """Initialize T with a kernel function.
-        Args:
-            kernel: Kernel function to use.
-        """
-        self.kernel = kernel
+def T(kernel: KernelFunction, X: np.ndarray, Y: np.ndarray, Z: np.ndarray) -> float:
+    """Compute T(X, Y, Z) for the given data."""
 
-    def __call__(self, X: np.ndarray, Y: np.ndarray, Z: np.ndarray) -> float:
-        """Compute T(X, Y, Z) for the given data."""
+    n = X.shape[0]  # Amount of simulated samples
+    m = Z.shape[0]  # Amount of unknown sampled data
 
-        n = X.shape[0]  # Amount of simulated samples
-        m = Z.shape[0]  # Amount of unknown sampled data
+    assert X.shape == Y.shape
 
-        assert X.shape == Y.shape
+    # Naive implementation (but more readable)
+    total = 0
+    for i in range(n):
+        for j in range(n):
+            total += kernel(Z[j], Y[i]) - kernel(Z[j], X[i])
 
-        # Naive definition (but more readable)
-        total = 0
-        for i in range(n):
-            for j in range(n):
-                total += self.kernel(Z[j], Y[i]) - self.kernel(Z[j], X[i])
+    return total / (n * m)
 
-        return total / (n * m)
+
+def MMD2u(kernel: KernelFunction, X: np.ndarray, Y: np.ndarray) -> float:
+    """Compute squared MMDu for the given data."""
+
+    n = X.shape[0]
+    m = Y.shape[0]
+
+    total = 0
+
+    # Naive implementations, but readable
+    subtotal_x = 0
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                subtotal_x += kernel(X[i], X[j])
+    total += subtotal_x / (n * (n - 1))
+
+    subtotal_y = 0
+    for i in range(m):
+        for j in range(m):
+            if i != j:
+                subtotal_y += kernel(Y[i], Y[j])
+    total += subtotal_y / (m * (m - 1))
+
+    subtotal_mixed = 0
+    for i in range(n):
+        for j in range(m):
+            subtotal_mixed += kernel(X[i], Y[j])
+    total -= 2 * subtotal_mixed / (n * m)
+
+    return total
+
+
+def gamma(kernel: KernelFunction, X: np.ndarray, Y: np.ndarray, pi: float) -> float:
+    """Compute gamma for the given data."""
+
+    return pi * MMD2u(kernel, X, Y) + T(kernel, X, Y, X)
+
+
+def Phi(
+    kernel: KernelFunction, X: np.ndarray, Y: np.ndarray, Z: np.ndarray, pi: float
+) -> Literal[0, 1]:
+    """Compute the Phi hypothesis indicator."""
+
+    if T(kernel, X, Y, Z) >= gamma(kernel, X, Y, pi):
+        return 1
+    return 0
